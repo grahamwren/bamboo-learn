68ca6eb8bb6cb0c5a5f6868759e55c1cbaac2260
grahamwren <p4wren@gmail.com>
2017-11-12 18:38:23 -0500

    Added travis file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :courses
   root to: 'home#show'
 
   devise_for :users
 
   resources :users
+  resources :courses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
