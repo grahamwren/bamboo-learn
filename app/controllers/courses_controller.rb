0754dac0672fe0e96f78864859d0a72e82d5dd3f
grahamwren <p4wren@gmail.com>
2017-11-24 10:30:05 -0500

    Added edit view

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,4 +3,18 @@ class CoursesController < ApplicationController
     @courses = Course.all
   end
 
+  def edit
+    @course = Course.find(params[:id])
+    render 'edit'
+  end
+
+  def new
+    if current_user.admin?
+      @course = Course.new
+      render 'new'
+    else
+      redirect_to courses_path, status: :unauthorized, alert: "Access Denied"
+    end
+  end
+
 end
