800346e30090330d81e5a6482a5efc0f232af148
grahamwren <p4wren@gmail.com>
2017-11-24 09:50:55 -0500

    Added index tests for courses controller

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class CoursesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    Warden.test_mode!
+    @admin = users :admin
+    @teacher = users :teacher
+    @student = users :student_alex
+    @user_params = {
+        school_id: 666, user_type: 'student',
+        email: 'jessie@bamboo.com', first_name: 'jessie',
+        last_name: 'test', dob: Date.new(1970,1,1),
+        password: 'this is a terrible password'
+    }
+  end
+
+  teardown do
+    Warden.test_reset!
+  end
+
+  test 'Admin able to index all courses' do
+    login_as @admin, scope: :user
+    get courses_url
+    assert_response :success
+    assert_template :index
+  end
+
+  test 'Teacher able to index all courses' do
+    login_as @teacher, scope: :user
+    get courses_url
+    assert_response :success
+    assert_template :index
+  end
+
+  test 'Student able to index all courses' do
+    login_as @student, scope: :user
+    get courses_url
+    assert_response :success
+    assert_template :index
+  end
 end
