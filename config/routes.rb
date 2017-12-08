b62d1e70eeeadd042a02b8d549be5f8950bf6531
grahamwren <p4wren@gmail.com>
2017-12-08 17:12:44 -0500

    Text submissions other files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :submissions
   resources :assignments
   root to: 'home#show'
 
@@ -9,7 +8,9 @@ Rails.application.routes.draw do
   resources :courses do
     post :add_self, on: :member
     delete :drop_self, on: :member
-    resources :assignments, shallow: true
+    resources :assignments, shallow: true do
+      resources :submissions, shallow: true
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
